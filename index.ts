be4544be09faa265cc08a9dd9ee9a4b0e2cf0b17
x462751 <X462751@santanderglobaltech.com>
2022-06-27 10:19:35 +0200

    communication stablished

--- index.ts
+++ index.ts
@@ -16,14 +16,32 @@ const client = mqtt.connect(connectUrl, {
 });
 
 
-const topic = '/nodejs/mqtt';
+const topicAWeight = '/is1234/stat/actualweight';
+const topic_get_average = '/is1234/get/averageweight';
+
 client.on('connect', () => {
   console.log('Connected')
-  client.subscribe([topic], () => {
-    console.log(`Subscribe to topic '${topic}'`)
+  client.subscribe([topic_get_average], () => {
+    console.log(`Subscribe to topic '${topic_get_average}'`)
   })
+
+  setInterval(()=>{
+    sendAWeight(client);
+  }, 5000);
+  
 })
 
 client.on('message', (topic, payload) => {
     console.log('Received Message:', topic, payload.toString())
 })
+
+const sendAWeight = (client) => {
+
+  // REAL WEIGHT: 14.50 +- 0.1
+  const aWeight = 14.49 + Math.random()*2/10;
+  client.publish(topicAWeight, aWeight.toString(), { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+}
